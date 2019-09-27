4c7e05df226eb6841c55745f8a4c41e2eb2561a0
Mez Charney <mez.charney@gmail.com>
2019-09-27 23:45:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 
-def load_library(file)
-  library = YAML.load_file('emoticons.yml')
+def load_library(file_path)
+  YAML.load_file('emoticons.yml')
 end
 
 def get_japanese_emoticon
