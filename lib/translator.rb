1eb9ecdde6484170a8c949e56bbb16bfb00bf114
Mez Charney <mez.charney@gmail.com>
2019-09-28 00:50:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
  library = load_library(file_path)
  if library[:get_meaning].include?(emoticon)
-   library[:]
+   library[:get_meaning][emoticon]
 end
 
 def get_english_meaning
