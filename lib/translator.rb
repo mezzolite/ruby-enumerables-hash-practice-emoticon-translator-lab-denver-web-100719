e5cb4e9e12a96dd59c3e53bdae15f71c5e6559b0
Mez Charney <mez.charney@gmail.com>
2019-09-28 00:57:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,4 +23,9 @@ end
 
 def get_english_meaning(file_path, emoticon)
   library = load_library(file_path)
+  if library[:get_meaning].include?(emoticon)
+    library[:get_meaning][emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
